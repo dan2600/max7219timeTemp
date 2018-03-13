6206f64a0c8355b2e2ab3a43c0e746b4845de70b
Dan Spagnoli <Dan.spagnoli@vh1.com>
2018-03-13 14:34:44 -0400

    almost

--- app.py
+++ app.py
@@ -12,12 +12,18 @@ from luma.core.render import canvas
 from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
-
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0)
 print("Created device")
+
+def showMsg():
+    msg = theTime.strftime("%b %d %y %H:%M:%S")+" Current condition: "+theWeather[0].text()+"Current Temp "+theWeather[0].temp()+"° F"
+    print(msg)
+    show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0.1)
+
+
+
 theTime = internetTime.getDateTime()
 theWeather = internetWeather.getWeather()
-msg = "Current Temp "+theWeather[0].temp()+"° F"
-print(msg)
-show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0.1)
+showMsg();
+
